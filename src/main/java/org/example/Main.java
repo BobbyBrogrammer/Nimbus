6734b706dabfcaa2845b47ee719ccd9a271e789b
BobbyBrogrammer <robin.lindholm111@gmail.com>
2025-09-24 11:30:55 +0200

    Update to Nimbus

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.time.Period;
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+    //-------------------How old is Nimbus?-----------------------------------
+        LocalDate startDate = LocalDate.of(2025,5,17);
+        LocalDate today = LocalDate.now();
+        Period period = Period.between(startDate, today);
+
+        System.out.println("Nimbus is: " +period.getYears() + " years, "
+                + period.getMonths() + "months and "
+                + period.getDays() + " days old.");
+    //------------------------------------------------------------------------
+    }
+}
