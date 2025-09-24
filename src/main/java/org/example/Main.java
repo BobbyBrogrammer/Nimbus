f2bc5624281f5b46922397e58a9c3aefd369356a
BobbyBrogrammer <robin.lindholm111@gmail.com>
2025-09-24 11:51:15 +0200

    Updatetest

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,6 +13,7 @@ public class Main {
         System.out.println("Nimbus is: " +period.getYears() + " years, "
                 + period.getMonths() + "months and "
                 + period.getDays() + " days old.");
+        System.out.println("Update");
     //------------------------------------------------------------------------
     }
 }
